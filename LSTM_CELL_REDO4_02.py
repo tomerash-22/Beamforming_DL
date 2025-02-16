1919907c5a8b548df1d2476fc946d267a5f5dae2
tomerash-22 <tomerash@campus.technion.ac.il>
2025-02-16 12:04:25 +0200

    initial upload

---
+++ LSTM_CELL_REDO4_02.py
@@ -0,0 +1,276 @@
+import pickle
+
+import torch.nn.init as init
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import scipy.io
+import torch.optim as optim
+from tqdm import tqdm
+import numpy as np
+import torch
+from LSTM_models import SequentialRNNNet
+from Matlab_dataloaders import MATLABDataset_inter
+from gen_utils import save_torch_model,load_torch_model
+import optuna
+import optuna.visualization
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+batch_size = 1
+epochs = 10
+mat_file_path = 'IQ_400sig_10_02.mat'
+num_epochs = 20
+buffer_len = 1000
+sig_len = 80000
+backprop_const = 2
+n_train_examples = 100
+n_valid_examples = 40
+num_dir=2
+
+best_val_ratio=2
+
+# Define the objective function for Optuna optimization
+def objective(trial):
+    # Define the lists of possible values for the hyperparameters
+    sig_2fc_values = range(40,200,2)  # List of possible values for sig_2fc
+    backprop_const_val = range(1,100)
+    lr_vals = np.logspace(-7,-3,100)
+    input_size_vals = range(2,40,2)
+    proj_size_values = range(40,200,10)  # List of possible values for proj_size
+    cell_size_values = range(1000,3000,100) # List of possible values for cell_size
+    batch_size_vals = [20]
+    sced_fact_vals = [0.2,0.3,0.35,0.4,0.5,0.7]
+    #lr_vals =  [0.007]
+    # backprop_val = range(10)
+    RNN_type_vals = ['LSTM']#, 'RNN', 'GRU']
+
+    proj_size = trial.suggest_categorical("proj_size", proj_size_values)
+    cell_size = trial.suggest_categorical("cell_size", cell_size_values)
+    input_size = trial.suggest_categorical("input_size", input_size_vals)
+    sig2fc = trial.suggest_categorical("sig2fc", sig_2fc_values)
+    RNN_type = trial.suggest_categorical("rnn_type", RNN_type_vals)
+    learning_rate=trial.suggest_categorical("lr", lr_vals)
+    num_dir=trial.suggest_categorical("bi", [1,2])
+    backprop_const=trial.suggest_categorical("backprop",backprop_const_val)
+    batch_size=trial.suggest_categorical("batch_sz",batch_size_vals)
+    sced_fact = trial.suggest_categorical("sced_factor",sced_fact_vals)
+    model = SequentialRNNNet(input_size=input_size,proj_size=proj_size,sig_to_fc=sig2fc,
+                             RNNtype=RNN_type,cell_size=cell_size,batch_size=batch_size,bi=num_dir).to(device)
+
+# def train_network(mat_file_path, buffer_len,sig_len ,num_epochs, learning_rate=0.01, batch_size=1,\
+#                    validation_split=0.2,device='cuda' if torch.cuda.is_available() else 'cpu',
+#                   input_size=10,cell_size=1000,num_dir=2,sig_2fc=100,backprop_const=5,proj_size=100,
+#                   model_path = '8_02_IQnopass_h_and_c.pth',RNN_type='LSTM',optuna_trail=None):
+
+    dataset = MATLABDataset_inter(mat_file_path,signal_len=sig_len)
+    dataset.set_train()
+    train_dataset= dataset.get_train_set()
+    dataset.set_validation()
+    val_dataset=dataset.get_validation_set()
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+        # for optuna
+
+    #dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    #model = load_torch_model(model=model,path='10_02_backprop1_best_siglen1000 ' )
+    # Print the size of the model (total number of parameters)
+    # Calculate the total number of parameters (in bytes)
+    total_params = model.count_params()
+    for name, param in model.named_parameters():
+         print(f"{name}: {param.numel()} parameters")
+    # Assuming 4 bytes per parameter (float32)
+    model_size_bytes = total_params * 4
+    model_size_mb = model_size_bytes / (1024 ** 2)
+    print(f"Model size: {model_size_mb:.2f} MB")
+
+    if model_size_mb > 30 or proj_size>cell_size:
+        raise  optuna.exceptions.TrialPruned()
+    if sig2fc%input_size != 0:
+        raise optuna.exceptions.TrialPruned()
+
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate,weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=sced_fact, patience=0,cooldown=1)
+    step_interval = 2*sig2fc  # Initial interval size
+    interval_growth =0  # Growth factor for interval size
+    grad_flag = 0
+    lr=learning_rate
+    best_val = 10e8
+    for epoch in range(num_epochs):
+        new_lr = optimizer.param_groups[0]['lr']
+        if new_lr != lr:
+            lr=new_lr
+            print("lr is now" + str(lr))
+        model.train()
+        epoch_loss = 0.0
+        epoch_diff=0.0
+        sig_iter=0
+
+        #save_torch_model(model=model , path='10_02_backprop1')
+
+        for clean_sig , distor_sig in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}"):
+            clean_sig , distor_sig = clean_sig.to(device), distor_sig.to(device)
+            backprop_iter = 0
+            diff=0.0
+            sig_loss=0
+            if proj_size !=0:
+                h_0,c_0 =torch.ones(num_dir,batch_size ,proj_size), torch.ones(num_dir,batch_size ,cell_size)
+            else:
+                h_0, c_0 = torch.ones(num_dir, batch_size, cell_size), torch.ones(num_dir, batch_size, cell_size)
+            # Process the input signal in chunks
+            for t in range(0,sig_len,step_interval):
+                # Select the chunk for current interval
+                input_chunk =  distor_sig[:, t:t + step_interval]
+                target_chunk = clean_sig[:, t:t + step_interval]
+                backprop_iter += 1
+                # Forward pass
+                if backprop_iter == backprop_const:
+                    if RNN_type=='LSTM':
+                        output,h_0,c_0 = model(input_chunk,h_0,c_0 )
+                        h_0 , c_0 = h_0.detach(), c_0.detach()
+                    else:
+                        output, h_0 = model(input_chunk, h_0,c_0)
+                        h_0= h_0.detach()
+                else:
+                    if RNN_type == 'LSTM':
+                        output, h_0, c_0 = model(input_chunk, h_0, c_0)
+                    else:
+                        output, h_0 = model(input_chunk, h_0, c_0)
+
+                diff =  diff+ criterion(input_chunk,target_chunk)
+                # Calculate the loss for the current interval
+                # target_chunk = target_chunk.flatten()
+                loss = criterion(output,target_chunk)
+                # Accumulate the interval loss and backpropagate
+                sig_loss=sig_loss+loss
+
+                if backprop_iter == backprop_const :
+                    epoch_loss += sig_loss
+                    sig_loss.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    sig_loss,backprop_iter = 0,0
+
+            sig_iter+=1
+            epoch_diff += diff
+            epoch_loss += sig_loss
+            if sig_iter >= (n_train_examples/batch_size):
+                sig_iter=0
+                break
+
+        #avg_diff = epoch_diff / len(train_loader)
+        #avg_epoch_loss = epoch_loss /len(train_loader)
+        train_ratio = float(epoch_loss/epoch_diff)
+        #print(f"Epoch [{epoch + 1}/{num_epochs}], Avg Loss: {avg_epoch_loss:}")
+        print( "train_ratio" + str(train_ratio))
+
+
+ # Validation phase
+        model.eval()
+        val_loss = 0.0
+        diff_val=0.0
+        with (torch.no_grad()):
+            for clean_sig, distor_sig in tqdm(val_loader, desc=f"Epoch {epoch + 1}/{num_epochs}"):
+                clean_sig, distor_sig = clean_sig.to(device), distor_sig.to(device)
+
+                diff = 0.0
+                sig_loss = 0
+                if proj_size != 0:
+                    h_0, c_0 = torch.ones(num_dir, batch_size, proj_size), torch.ones(num_dir, batch_size, cell_size)
+                else:
+                    h_0, c_0 = torch.ones(num_dir, batch_size, cell_size), torch.ones(num_dir, batch_size, cell_size)
+
+                # Process the input signal in chunks
+                for t in range(0, sig_len, step_interval):
+                    # Select the chunk for current interval
+                    input_chunk = distor_sig[:, t:t + step_interval]
+                    target_chunk = clean_sig[:, t:t + step_interval]
+
+                    # Forward pass
+                    if RNN_type == 'LSTM':
+                        output, h_0, c_0 = model(input_chunk, h_0, c_0)
+                    else:
+                        output, h_0 = model(input_chunk, h_0, c_0)
+
+                    diff = diff + criterion(input_chunk, target_chunk)
+                    # Calculate the loss for the current interval
+                    #target_chunk = target_chunk.flatten()
+                    loss = criterion(output, target_chunk)
+                    # Accumulate the interval loss and backpropagate
+                    sig_loss = sig_loss + loss
+                diff_val += diff
+                val_loss += sig_loss
+                sig_iter += 1
+                if  sig_iter >= ( n_valid_examples/batch_size):
+                    sig_iter = 0
+                    break
+            #avg_val_loss = val_loss / len(val_loader)
+            val_ratio = float(val_loss/diff_val)
+            #print(f"Epoch [{epoch + 1}/{num_epochs}], Avg Validation Loss: {avg_val_loss:}")
+            print("val ratio" + str(val_ratio))
+            val_metric = float(val_ratio) #+ float(model_size_mb/10)
+
+            # if val_metric < best_val_ratio:
+            #     save_torch_model(model=model, path='noproj_first_scan_best.pth')
+            #     best_val_ratio = val_metric
+
+
+        trial.report(val_metric ,epoch)
+        if trial.should_prune():
+            raise optuna.exceptions.TrialPruned()
+        scheduler.step(val_metric)
+            # else:
+            #     backprop_const+=1
+            #     print("backward prog is cal every" + str(backprop_const) + 'steps' )
+
+        # Increase the step interval for the next epoch
+
+    return val_metric
+
+
+
+
+# Example usage within the training function:
+# After training is complete, save two signals and their outputs
+# Replace 'mat_file_path' and 'signal_len' with appropriate values
+#  print("Training complete.")
+ #   torch.save(model.state_dict(), 'trained_lstm_net.pth')
+#    print("Model saved as 'trained_lstm_net.pth'.")
+# Example usage
+# Create and optimize using Optuna
+
+
+timeout= 60*60*10
+study = optuna.create_study(direction='minimize')  # You can change the direction depending on the objective
+study.enqueue_trial({"proj_size":50,"cell_size":1900 ,  "input_size":6,"sig2fc":84,
+                      "rnn_type":'LSTM' , "backprop":35 , "batch_sz":20, "bi":1 ,
+                      "sced_factor":0.5})
+study.optimize(objective, n_trials=1000 ,timeout=timeout)  # Number of trials to perform
+
+with open ("NO_PROJ_RNN_siglen10K_5epoch.pkl",'wb') as f:
+    pickle.dump(study,f)
+
+
+
+pruned_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.PRUNED]
+complete_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.COMPLETE]
+print("Study statistics: ")
+print(" Number of finished trials: ", len(study.trials))
+print(" Number of pruned trials: ", len(pruned_trials))
+print(" Number of complete trials: ", len(complete_trials))
+print("Best trial:")
+trial = study.best_trial
+print(" Value: ", trial.value)
+print(" Params: ")
+for key, value in trial.params.items():
+ print(" {}: {}".format(key, value))
+
+fig = optuna.visualization.plot_param_importances(study)
+fig.show()
+
+
+
+#
+# train_network(mat_file_path='IQ_400sig_10_02.mat', num_epochs=50, batch_size=1 ,\
+#               learning_rate=0.0005,buffer_len=1000,sig_len=10000,proj_size=50,cell_size=2000
+#               ,backprop_const=3,input_size=10)
---
+++ Matlab_dataloaders.py
@@ -0,0 +1,148 @@
+
+import torch.nn.init as init
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import scipy.io
+import torch.optim as optim
+from tqdm import tqdm
+import numpy as np
+import torch
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+
+class MATLABDataset_inter(Dataset):
+    def __init__(self, mat_file, signal_len,split_ratio=0.8):
+        """
+        Args:
+            mat_file (str): Path to the .mat file.
+            signal_len (int): Desired constant length for all signals.
+        """
+        mat_data = scipy.io.loadmat(mat_file)
+
+        self.clean_signals = [sig.flatten() for sig in mat_data['IQ_dataset_param']['clean_sig'][0][0][0]]
+        self.distor_signals = [sig.flatten() for sig in mat_data['IQ_dataset_param']['distor_sig'][0][0][0]]
+
+        # Split into train and validation sets
+        split_idx = int(len(self.clean_signals) * split_ratio)
+        self.train_clean_signals = self.clean_signals[:split_idx]
+        self.train_distor_signals = self.distor_signals[:split_idx]
+
+        self.val_clean_signals = self.clean_signals[split_idx:]
+        self.val_distor_signals = self.distor_signals[split_idx:]
+
+
+    def _truncate_or_pad(self, signal, target_len):
+        """
+        Truncates or pads the signal to the target length.
+        """
+        if len(signal) > target_len:
+            return signal[:target_len]  # Truncate
+        elif len(signal) < target_len:
+            return np.pad(signal, (0, target_len - len(signal)), 'constant')  # Pad
+        return signal
+
+    def set_train(self):
+        """
+        Set the dataset to training mode.
+        """
+        self.clean_signals = self.train_clean_signals
+        self.distor_signals = self.train_distor_signals
+
+    def set_validation(self):
+        """
+        Set the dataset to validation mode.
+        """
+        self.clean_signals = self.val_clean_signals
+        self.distor_signals = self.val_distor_signals
+
+    def get_train_set(self):
+        """
+        Returns the training dataset in the format expected by the network.
+        """
+        return [self[i] for i in range(len(self.train_clean_signals))]
+
+    def get_validation_set(self):
+        """
+        Returns the validation dataset in the format expected by the network.
+        """
+        return [self[i] for i in range(len(self.val_clean_signals))]
+
+
+    def __len__(self):
+        return len(self.clean_signals)
+
+    def __getitem__(self, idx):
+        # Get real and imaginary parts of clean and distorted signals
+        clean_signal = self.clean_signals[idx]
+        distor_signal = self.distor_signals[idx]
+
+        clean_real = self._scale(torch.tensor(np.real(clean_signal), dtype=torch.float32))
+        clean_imag = self._scale(torch.tensor(np.imag(clean_signal), dtype=torch.float32))
+        distor_real = self._scale(torch.tensor(np.real(distor_signal), dtype=torch.float32))
+        distor_imag = self._scale(torch.tensor(np.imag(distor_signal), dtype=torch.float32))
+
+        clean_sig = self._interleave(clean_real,clean_imag)
+        distor_sig = self._interleave(distor_real, distor_imag)
+
+        # Pack in a way that matches what the network expects
+        return clean_sig,distor_sig
+
+    def _interleave(self,real_sig,imag_sig):
+        interleaved = torch.empty(2 *  real_sig.size(0), dtype=real_sig.dtype)
+        interleaved[0::2] = real_sig
+        interleaved[1::2] = imag_sig
+        return interleaved
+
+
+    def _scale(self, sig):
+        min = sig.min()
+        max = sig.max()
+        norm_01 = (sig - min) / (max - min)
+        norm_100 = norm_01 * 200 - 100
+        return norm_100
+
+class MATLABDataset(Dataset):
+    def __init__(self, mat_file, signal_len):
+        """
+        Args:
+            mat_file (str): Path to the .mat file.
+            signal_len (int): Desired constant length for all signals.
+        """
+        mat_data = scipy.io.loadmat(mat_file)
+
+        self.clean_signals = [sig.flatten() for sig in mat_data['IQ_dataset_param']['clean_sig'][0][0][0]]
+        self.distor_signals = [sig.flatten() for sig in mat_data['IQ_dataset_param']['distor_sig'][0][0][0]]
+
+    def _truncate_or_pad(self, signal, target_len):
+        """
+        Truncates or pads the signal to the target length.
+        """
+        if len(signal) > target_len:
+            return signal[:target_len]  # Truncate
+        elif len(signal) < target_len:
+            return np.pad(signal, (0, target_len - len(signal)), 'constant')  # Pad
+        return signal
+
+    def __len__(self):
+        return len(self.clean_signals)
+
+    def __getitem__(self, idx):
+        # Get real and imaginary parts of clean and distorted signals
+        clean_signal = self.clean_signals[idx]
+        distor_signal = self.distor_signals[idx]
+
+        clean_real = self._scale(torch.tensor(np.real(clean_signal), dtype=torch.float32))
+        clean_imag = self._scale(torch.tensor(np.imag(clean_signal), dtype=torch.float32))
+        distor_real = self._scale(torch.tensor(np.real(distor_signal), dtype=torch.float32))
+        distor_imag = self._scale(torch.tensor(np.imag(distor_signal), dtype=torch.float32))
+
+        # Pack in a way that matches what the network expects
+        return distor_real, distor_imag, clean_real, clean_imag
+
+    def _scale(self, sig):
+        min = sig.min()
+        max = sig.max()
+        norm_01 = (sig - min) / (max - min)
+        norm_100 = norm_01 * 200 - 100
+        return norm_100
+
